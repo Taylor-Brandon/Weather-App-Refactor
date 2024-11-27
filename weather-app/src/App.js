e1b860c5996f110222e3c52f2977c816a4d5d97f
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-11-27 13:47:49 -0500

    added app.js content

---
+++ weather-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    )
+}
