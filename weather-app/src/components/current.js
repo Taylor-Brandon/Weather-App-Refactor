7456aca4d8848847ffd49a80e844e6655dd0d526
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2025-01-07 17:11:47 -0500

    fixed results styling and added styling to current weather

--- weather-app/src/components/current.js
+++ weather-app/src/components/current.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-export default function Current({ title, date, weather, temp, humidity, wind }) {
+export default function Current({ title, date, weather, temp, humidity, wind, icon }) {
     return (
+        <div className='weather-area'>
         <div className="weather-info">
-            <h2>{title}</h2>
-            <p>{date && new Date(date).toLocaleString()}</p>
+            <h2 className='weather-title'>{title} ({date})</h2>
             <p>Weather: {weather}</p>
             <p>Temperature: {temp}°C</p>
             <p>Humidity: {humidity}%</p>
             <p>Wind Speed: {wind} m/s</p>
+            <p>{icon}</p>
+        </div>
         </div>
     );
 }
