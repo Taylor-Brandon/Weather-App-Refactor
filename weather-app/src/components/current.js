46532fbcd7239221e47cf67149d5f29997af822f
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2025-01-08 14:28:43 -0500

    added icons to future and current weather conditions

--- weather-app/src/components/current.js
+++ weather-app/src/components/current.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function Current({ title, date, weather, temp, humidity, wind, icon }) {
+    const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
     return (
         <div className='weather-area'>
         <div className="weather-info">
             <h2 className='weather-title'>{title} ({date})</h2>
-            <p>Weather: {weather}</p>
+            <img src={iconUrl} alt={weather} />
             <p>Temperature: {temp}°C</p>
             <p>Humidity: {humidity}%</p>
             <p>Wind Speed: {wind} m/s</p>
